9b7c0b71c4e5118f51fecec4b3e30b8e657f0f00
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-09-12 11:59:03 +0600

    burj al arab

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB15ZmNFam6sNvfKEYc7vbA5W3feCk0bsc",
+    authDomain: "react-burj-al-arab.firebaseapp.com",
+    databaseURL: "https://react-burj-al-arab.firebaseio.com",
+    projectId: "react-burj-al-arab",
+    storageBucket: "react-burj-al-arab.appspot.com",
+    messagingSenderId: "212522895810",
+    appId: "1:212522895810:web:8ed64f6259c870e737ee5b"
+  };
+
+  export default firebaseConfig;
